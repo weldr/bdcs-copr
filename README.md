1f97b2d654bf979e01ddef357487309d9b427a2e
David Shea <dshea@redhat.com>
2017-07-26 15:53:52 -0400

    Add a script to package the dependencies for bdcs.
    This script builds all packages locally using mock, since COPR cannot
    currently build Haskell packages. Presumably once that gets working the
    mock calls could be replaced with COPR CLI calls.
    
    This is based on the packaging instructions provided by Chris Lumens.

---
+++ README.md
@@ -0,0 +1,4 @@
+Packages the dependencies of bdcs that are not already packages in Fedora
+
+The makedeps.sh script will package everything needed to build bdcs and spit
+out a bunch of spec files, also stored in this repo.
---
+++ makedeps.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+# REQUIRES:
+# cabal-rpm
+# mock
+# yum (https://bugzilla.redhat.com/show_bug.cgi?id=1474513)
+
+# This script will, hopefully, build a repository containing all the dependencies
+# needed for bdcs.
+
+BASEDIR="${PWD}"
+
+# directory for intermediate files
+WORKDIR="${PWD}/build"
+
+# directory for the finished repo
+REPODIR="${PWD}/repo"
+
+# SRPMs
+SRPMDIR="${PWD}/srpms"
+
+### FUNCTIONS
+runmock() {
+    # We need to use --old-chroot because of https://bugzilla.redhat.com/show_bug.cgi?id=1467299
+    mock --configdir "${WORKDIR}/config" -r weldr --old-chroot --resultdir "${WORKDIR}/results" "$@" || exit 1
+}
+
+
+# Convert a hackage package name to a rpm package name
+# *usually* this is just "whatever" -> "ghc-whatever", but packages that provide
+# executables don't get the ghc prefix, and the tests and rules are hard to reproduce
+# here, and the function that does this is only exposed in cabal-rpm via
+# writing out a spec file. So just hard code some known exec packages here and
+# maybe file a request against cabal-rpm later.
+rpmname() {
+    local cabalname="$1"
+
+    if [ "$cabalname" = "hsc2hs" ]; then
+        echo "$1"
+    else
+        echo "ghc-$1"
+    fi
+}
+
+# Build a package in mock
+buildpkg() {
+    local pkgname="$1"
+    local specfile="${pkgname}.spec"
+    
+    # Download the sources
+    spectool --gf "$specfile" -C "${WORKDIR}/sources" || exit 1
+
+    # Start with a fresh results directory
+    rm -rf "${WORKDIR}/results"/*
+
+    # Run mock
+    runmock --buildsrpm --spec "$specfile" --sources "${WORKDIR}/sources"
+    runmock --rebuild "${WORKDIR}/results/${pkgname}-"*.src.rpm
+
+    # Save the results
+    mv "${WORKDIR}/results/"*.src.rpm "${SRPMDIR}" || exit 1
+    mv "${WORKDIR}/results/"*.rpm "${REPODIR}" || exit 1
+    createrepo_c "${REPODIR}" || exit 1
+
+    # dnf unhelpfully caches the metadata from the local repo, so expire the cache
+    # each time the local repo is updated
+    dnf clean expire-cache
+}
+
+# Test whether a package is already available or not
+available() {
+    local pkgname="$1"
+
+    local result_count="$(dnf repoquery -q --whatprovides "$pkgname" --repofrompath weldr,"$REPODIR" | wc -l)"
+
+    if [ "$result_count" -eq 0 ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Find and build the unavailable dependencies for a package
+builddeps() {
+    local cabalname="$1"
+    local pkgname="$(rpmname "${cabalname}")"
+
+    # Loop through the dependencies
+    # cabal-rpm fails with weird messages sometimes so try to catch that
+    local deplist
+    deplist="$(cabal-rpm depends "${cabalname}")"
+    if [[ "$?" -ne 0 ]]; then
+        exit 1
+    fi
+
+    # two exits so that a failure from the while loop subshell actually gets caught
+    echo "${deplist}" | grep -v 'in Hackage$' | grep -v '\.pc$' | grep -Fv 'libpthread.so' | while read -r dep ; do
+        available "$(rpmname "${dep}")" || rebuild "${dep}" || exit 1
+    done || exit 1
+}
+
+# Build a package and its dependencies
+rebuild() {
+    local cabalname="$1"
+    local pkgname="$(rpmname "${cabalname}")"
+
+    # if it's already built, skip it
+    if [ -f "${REPODIR}/${pkgname}-"*.rpm ]; then
+        return 0
+    fi
+
+    builddeps "$cabalname"
+
+    # Spit out a spec file and build it
+    ( cd "$BASEDIR" &&
+      cabal-rpm spec "${cabalname}" &&
+      fixspec "${pkgname}" &&
+      buildpkg "${pkgname}" ) || exit 1
+}
+
+# Fix problems in the generated spec files
+fixspec() {
+    local pkgname="$1"
+
+    if [[ "$pkgname" = "ghc-haskell-gi" ]]; then
+        sed -i '/mv %{buildroot}%{_ghcdocdir}\/{,ghc-}%{name}/d' ghc-haskell-gi.spec
+    elif [[ "$pkgname" =~ ^ghc-gi- ]]; then
+        # cabal-rpm doesn't think there's a base package, since there's no exposed-modules,
+        # so we need to add the %files section and a Requires from -devel
+        # This kind of makes the spec file look weird but who cares
+        sed -i -e '/^%package devel/a\
+Requires: %{name}%{?_isa} = %{version}-%{release}' -e '$a\
+\
+%files -f %{name}.files\
+%license LICENSE' "${pkgname}.spec"
+    elif [[ "$pkgname" = "ghc-persistent-sqlite" ]]; then
+        # Build persistent-sqlite against the system sqlite library instead of the one it ships with
+        sed -i -e '1i\
+%global cabal_configure_options -fsystemlib\
+BuildRequires: sqlite-devel' "${pkgname}.spec"
+    else
+        return 0
+    fi
+}
+
+# Setup the directories
+mkdir -p "${WORKDIR}/config"
+mkdir -p "${WORKDIR}/sources"
+mkdir -p "${WORKDIR}/results"
+mkdir -p "${REPODIR}"
+mkdir -p "${SRPMDIR}"
+createrepo_c "${REPODIR}"
+dnf clean expire-cache
+
+# Create the config file
+cp "${PWD}/mock.cfg" "${WORKDIR}/config/weldr.cfg"
+echo "
+[weldr]
+name=weldr
+baseurl=file://$REPODIR/
+enabled=1
+gpgcheck=0
+\"\"\"
+
+config_opts['plugin_conf']['bind_mount_opts']['dirs'].append(('$REPODIR', '$REPODIR'))" >> "${WORKDIR}/config/weldr.cfg"
+
+
+# Before anything, need an up-to-date package list
+cabal update
+
+# Build some newer versions of dependencies
+rebuild memory
+rebuild gitrev
+rebuild cryptonite
+
+# Download bdcs and build its deps
+( cd build &&
+  rm -rf bdcs &&
+  git clone https://www.github.com/weldr/bdcs &&
+  cd bdcs/importer &&
+  builddeps db 
+)
